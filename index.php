b5973a90d174b394468b1ce74553e4cf913d2ca3
sosinatolossa <sosinatolossa16@gmail.com>
2021-07-27 20:55:08 -0500

    project done

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+$initial = '555';
+$a = octdec($initial);
+echo $a;
+echo "\n";
+
+$b = deg2rad($a);
+echo $b;
+echo "\n";
+
+$c = cos($b);
+echo $c;
+echo "\n";
+
+$d = round($c, 3);
+echo $d;
+echo "\n";
+
+$e = log($d);
+echo $e;
+echo "\n";
+
+$f = abs($e);
+echo $f;
+echo "\n";
+
+$g = acos($f);
+echo $g;
+echo "\n";
+
+$h = rad2deg($g);
+echo $h;
+echo "\n";
+
+$i = floor($h);
+echo $i;
+echo "\n";
+
+$j = $i - 47;
+echo $j;
+
+
+
+
